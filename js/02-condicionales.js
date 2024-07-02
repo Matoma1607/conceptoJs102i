4894b7dbc43ff0bc59813249e07e4a4927bb0575
Matias Salazar <matias39974593@gmail.com>
2024-07-01 23:28:53 -0300

    agregamos un ejemplo de condicional simple

---
+++ js/02-condicionales.js
@@ -0,0 +1,15 @@
+// Condicionales 
+
+/*
+if (condicion logica){
+todas las lineas que quieran que sucedan si se cumple la comndicion logica
+}
+*/
+
+// solicitar al usuario ingresar un numero y verificar si es positivo
+
+const numeroSolicitado = parseInt(prompt('ingrese un numero entero'));
+
+if (numeroSolicitado > 0){
+    document.write(`<p>El numero ingresado: ${numeroSolicitado} es positivo </p>`);
+}
