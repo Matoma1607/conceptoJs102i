fb5658f52d4b91c6ef52399ade80c33cf8f93c07
Matias Salazar <matias39974593@gmail.com>
2024-07-05 18:49:24 -0300

    se realizo un ejemplo de la estructura Wile

---
+++ js/03-bucles.js
@@ -0,0 +1,15 @@
+// Es una estructura repetitiva
+// varios tipos de bucles:
+// Estructura Wile: 
+
+/* Wile (condicion logica){
+aqui se agrega todo el codigonque quiero repetir
+agregar codigo que haga que la condicion logica se deje de cumplir
+}
+*/
+let renglon = 1
+
+while(renglon <= 5){
+    document.write (`<p>Renglon numero ${renglon} </p>`)
+    renglon = renglon + 1
+}
