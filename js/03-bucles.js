f57064b6874180654549078940ade1bdea83937c
Matias Salazar <matias39974593@gmail.com>
2024-07-05 23:16:54 -0300

    se agregó ejemplo de la estructura doWhile

--- js/03-bucles.js
+++ js/03-bucles.js
@@ -1,15 +1,28 @@
 // Es una estructura repetitiva
 // varios tipos de bucles:
-// Estructura Wile: 
+// Estructura Wile:
 
 /* Wile (condicion logica){
 aqui se agrega todo el codigonque quiero repetir
 agregar codigo que haga que la condicion logica se deje de cumplir
 }
 */
-let renglon = 1
+let renglon = 11;
 
-while(renglon <= 5){
-    document.write (`<p>Renglon numero ${renglon} </p>`)
-    renglon = renglon + 1
+while (renglon <= 10) {
+  document.write(`<p>Renglon numero ${renglon} </p>`);
+  renglon++; //renglon = renglon + 1;
 }
+
+// Estructura Do-Wile:
+/* Do{
+aqui se agrega todo el codigonque quiero repetir
+agregar codigo que haga que la condicion logica se deje de cumplir
+}Wile(condicion logica)
+*/
+
+let linea = 1;
+do {
+  document.write(`<p>Renglon numero ${linea} </p>`);
+  linea++;
+} while (linea <= 5);
